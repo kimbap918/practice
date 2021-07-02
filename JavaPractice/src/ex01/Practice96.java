76765aaa28e73bd41a9c2bab1a909e1aeaf34b05
kimbap <fx887722@naver.com>
2021-07-03 00:20:01 +0900

    java 다차원 배열

---
+++ JavaPractice/src/ex01/Practice96.java
@@ -0,0 +1,53 @@
+package ex01;
+import java.util.*;
+
+public class Practice96 {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) {
+		final int SIZE = 10;
+		int x = 0, y = 0;
+		
+		char[][] board = new char[SIZE][SIZE];
+		byte[][] shipBoard = {
+				{ 0, 0, 0, 0, 0, 0, 1, 0, 0 }, //1
+				{ 1, 1, 1, 1, 0, 0, 1, 0, 0 }, //2
+				{ 0, 0, 0, 0, 0, 0, 1, 0, 0 }, //3
+				{ 0, 0, 0, 0, 0, 0, 1, 0, 0 }, //4
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //5
+				{ 1, 1, 0, 1, 0, 0, 0, 0, 0 }, //6
+				{ 0, 0, 0, 1, 0, 0, 0, 0, 0 }, //7
+				{ 0, 0, 0, 1, 0, 0, 0, 0, 0 }, //8
+				{ 0, 0, 0, 0, 0, 1, 1, 1, 0 }, //9
+				};
+		
+		for(int i=1;i<SIZE;i++)
+			board[0][i] = board[i][0] = (char)(i+'0');
+		
+		while(true) {
+			System.out.printf("좌표를 입력하세요.(종료는 00)>");
+			String input = sc.nextLine();
+			
+			if(input.length()==2) {
+				x = input.charAt(0) - '0';
+				y = input.charAt(1) - '0';
+				
+				if(x==0 && y==0)
+					break;
+			}
+			
+			if(input.length()!=2 || x<=0 || x>=SIZE || y<=0 || y>=SIZE) {
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+				continue;
+			}
+			
+			board[x][y] = shipBoard[x-1][y-1]==1 ? 'O' : 'X';
+			
+			for(int i=0;i<SIZE;i++) 
+				System.out.println(board[i]);
+			System.out.println();
+		}
+	}
+	}
+
+}
