7da30217e92d5ce2f6bab12cbbeeb9a16a707a57
kimbap <fx887722@naver.com>
2021-07-13 23:59:22 +0900

    java 연습 상속

---
+++ JavaPractice/src/ex01/InheritanceTv.java
@@ -0,0 +1,32 @@
+package ex01;
+
+class Tv {
+	boolean power;
+	int channel;
+	
+	void power() {	power = !power; }
+	void channelUp() {	++channel; }
+	void channelDown() { --channel; }
+}
+
+class CaptionTv extends Tv {
+	boolean caption;
+	void displayCaption(String text) {
+		if(caption) {
+			System.out.println(text);
+		}
+	}
+}
+
+public class InheritanceTv {
+	public static void main(String[] args) {
+		CationTv ctv = new CaptionTv();
+		ctv.channel = 10;
+		ctv.channelUp();
+		System.out.println(ctv.channel);
+		ctv.displayCaption("Hello, World");
+		ctv.caption=true;
+		ctv.displayCaption("Hello, World");
+	}
+
+}
